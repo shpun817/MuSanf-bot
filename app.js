90c78646c4bec5885a6bb15e4999028ee2400512
Sanford Pun <punsanford@gmail.com>
2021-03-14 00:57:17 +0800

    First commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const Discord = require('discord.js');
+require('dotenv').config();
+
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log("MuSanf is ready")
+});
+
+client.on('message', (msg) => {
+    if (msg.content === 'Hello') msg.reply('Hi');
+});
+
+client.login(process.env.BOT_TOKEN);
