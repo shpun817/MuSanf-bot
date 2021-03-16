175402daa82b79952250eb424ee1dea50d2616a0
Sanford Pun <punsanford@gmail.com>
2021-03-16 18:24:58 +0800

    Improve reset function

--- src/msg_func/reset.ts
+++ src/msg_func/reset.ts
@@ -3,19 +3,20 @@ import { Player } from '../player';
 import { SongQueue } from '../song_queue';
 
 export async function reset(msg: Discord.Message | null, args: string[]) {
-    msg?.guild.me.voice.channel.leave();
+    Player.voiceChannelConnection?.disconnect();
     const empty: boolean = args.length === 0;
     const silent: boolean = args.includes('silent');
+    await msg?.channel.send("*Resetting......*");
     if (empty || args.includes('player')) {
         Player.reset();
-        if (!silent && msg !== null) {
-            await msg.channel.send("Mom just got me a new Player!");
+        if (!silent) {
+            await msg?.channel.send("Mom just got me a new Player!");
         }
     }
     if (empty || args.includes('queue')) {
         SongQueue.clear();
-        if (!silent && msg !== null) {
-            await msg.channel.send("The song queue is good as *new*.");
+        if (!silent) {
+            await msg?.channel.send("The song queue is good as *new*.");
         }
     }
     return;
