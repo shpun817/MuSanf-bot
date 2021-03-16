da679bacd5b1bbb34df369bc81a746796a0f65db
Sanford Pun <punsanford@gmail.com>
2021-03-16 16:56:56 +0800

    Reset all

--- src/msg_func/reset.ts
+++ src/msg_func/reset.ts
@@ -3,17 +3,16 @@ import { Player } from '../player';
 import { SongQueue } from '../song_queue';
 
 export async function reset(msg: Discord.Message | null, args: string[]) {
-    if (msg !== null) {
-        msg.guild.me.voice.channel.leave();
-    }
+    msg?.guild.me.voice.channel.leave();
+    const empty: boolean = args.length === 0;
     const silent: boolean = args.includes('silent');
-    if (args.includes('player')) {
+    if (empty || args.includes('player')) {
         Player.reset();
         if (!silent && msg !== null) {
             await msg.channel.send("Player is reset.");
         }
     }
-    if (args.includes('queue')) {
+    if (empty || args.includes('queue')) {
         SongQueue.clear();
         if (!silent && msg !== null) {
             await msg.channel.send("The song queue has been cleared.");
